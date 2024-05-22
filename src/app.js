526489ef78081307b95940021d1dabd7faa77ad4
Juan Jose Chiroque <juanjosechiroque@gmail.com>
2024-05-22 12:08:48 -0500

    feat: add helmet

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 import router from "./router.js";
 import cors from "cors";
 import { errorGenericHandler } from "./middleware.js";
+import helmet from "helmet";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(router);
